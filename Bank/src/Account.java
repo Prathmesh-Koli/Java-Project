424de4a268ded5016691b2173540c7982293dd38
Prathmesh-Koli <prathmeshkoli759@gmail.com>
2023-12-16 20:54:20 +0530

    push

---
+++ Bank/src/Account.java
@@ -0,0 +1,9 @@
+
+public class Account {
+       void acc(int no1, int no2, int no3, int n) {
+      
+    	     int total = no1 + no2 + no3 + n;
+    	     
+    	     System.out.println("Total Account : " + total);
+       }
+}
---
+++ Bank/src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Account a1 = new Account();
+		a1.acc(1, 2, 3, 4);
+	}
+
+}
